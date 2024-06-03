038310de5fb234d70383e12e51dd576005861d44
superuser-parth <yogaonkar@gmail.com>
2024-06-03 21:35:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from ultralytics import YOLO
+from flask import request, Response, Flask
+from waitress import serve
+from PIL import Image
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    """
+    Site main page handler function.
+    :return: Content of index.html file
+    """
+    with open("index.html") as file:
+        return file.read()
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    """
+        Handler of /detect POST endpoint
+        Receives uploaded file with a name "image_file", 
+        passes it through YOLOv8 object detection 
+        network and returns an array of bounding boxes.
+        :return: a JSON array of objects bounding 
+        boxes in format 
+        [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    buf = request.files["image_file"]
+    boxes = detect_objects_on_image(Image.open(buf.stream))
+    return Response(
+      json.dumps(boxes),  
+      mimetype='application/json'
+    )
+
+
+def detect_objects_on_image(buf):
+    model = YOLO("best.pt")
+    results = model.predict(buf)
+    result = results[0]
+    output = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
+        class_id = box.cls[0].item()
+        prob = round(box.conf[0].item(), 2)
+        class_name = result.names[class_id]
+        if class_name == 'Cavity':
+            class_name = 'Caries'
+        output.append([x1, y1, x2, y2, class_name, prob])
+    print(f"Detected boxes: {output}")  # Add this line to log detected boxes
+    return output
+
+
+
+serve(app, host='0.0.0.0', port=8081)
